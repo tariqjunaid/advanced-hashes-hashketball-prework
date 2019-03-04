fbe36984f829b9b472bb49853212377794fd1737
Junaid Tariq <junaidtariq_91@yahoo.com>
2019-03-04 03:15:48 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -213,7 +213,7 @@ def long_name_steals_a_ton?
   longest = {}
   game_hash.each do |team, data|
     data[:players].each do |p_name, p_data|
-      longest[p_data[:player_name]] = p_name[:steals]
+      longest[p_name[:player_name]] = p_data[:steals]
     end
   end
   max_steals = longest.values.max
