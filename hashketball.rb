1935959c2eb2376d1f1500ac4d7640b13a6a7ae3
Junaid Tariq <junaidtariq_91@yahoo.com>
2019-03-04 02:46:30 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -202,7 +202,7 @@ end
 def player_with_longest_name
   longest_name = []
   game_hash.each do |team, data|
-    data[:players].each do |p_name|
+    data[:players].each do |p_name, p_data|
       longest_name << p_name
     end
   end
